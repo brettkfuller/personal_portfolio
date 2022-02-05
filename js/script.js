86cc2dcda0fcce7bb29c51629e736f52967aa87c
Brett Fuller <brettkfuller95@gmail.com>
2022-02-04 16:50:02 -0800

    Finished main webpage design

--- js/script.js
+++ js/script.js
@@ -75,3 +75,26 @@ new Typewriter('#typewriter', {
   cursor: "|"
 });
 console.log("Typewriter effect is working")
+
+
+//======================== :Portfolio Swiper Effect =======================
+
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  pagination: {
+    el: "blog-slider__pagination",
+    clickable: true,
+  },
+  // mousewheel: true,
+  keyboard: true,
+});
+console.log("Portfolio swiper is working!")
